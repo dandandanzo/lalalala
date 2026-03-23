ae76f91d42cc55743f40e6981ab0de5f2359656d
dandandanzo <jrdntio@gmail.com>
2026-03-23 08:50:48 +0800

    Update and rename dandandandan to dandandandan.lua

--- dandandandan.lua
+++ dandandandan.lua
@@ -1,5 +1,7 @@
 script_key="hsyQEYXbFmWuTvLhCbxxwtIZfsDgpMus";
 getgenv().SailorPieceConfig = {
+    ---------- [ FARMING ] ----------
+    AutoRejoinIfHighRam = 5000,
     AutoFarm         = true,          -- Auto đánh mob + làm quest
     AutoQuest        = true,          -- Auto nhận/hoàn thành quest
     AutoStats        = true,          -- Auto nâng stat (Melee -> Sword/Power/Defense)
@@ -8,6 +10,7 @@ getgenv().SailorPieceConfig = {
     AutoIchigo       = true,          -- Auto exchange Ichigo khi đủ 500 Boss Ticket
     AutoHaki         = true,          -- Auto farm quest Haki 
     AutoHogyoku      = true,          -- Auto làm Hogyoku quest unlock Soul Society (lv8500+)
+    AutoDungeon      = true,          -- Auto làm Dungeon quest unlock (lv5000+)
     AutoAscend       = true,          -- Auto ascend khi đủ điều kiện
     AutoRedeemCodes  = true,          -- Auto nhập code
     DisablePvP       = true,          -- Tắt PvP trong settings
@@ -29,10 +32,10 @@ getgenv().SailorPieceConfig = {
     AutoEquipAccessory = true,        -- Auto equip accessory mạnh nhất
     AutoEquipArtifact = true,         -- Auto equip artifact rarity cao nhất per slot
     AutoSkillTree    = true,          -- Auto unlock & upgrade skill tree
-    SkillTreePriority = {"Luck", "Damage", "CritChance", "CritDamage", "HP"}, -- Thứ tự ưu tiên upgrade
+    SkillTreePriority = {"Luck", "Damage", "CritChance", "CritDamage", "HP"},
     WebhookURL       = "https://discord.com/api/webhooks/1478134572867911691/Z5WpUhdJkcdBSsoYE-k79YovaVsxNn93aN7TqRrGu7VFEXh_Lsm0znIaRln5DNbSquoG",             -- Discord Webhook URL (để trống = tắt)
     FPSBoost         = true,          -- Tối ưu FPS (giảm lag)
-    FPSLock          = 15,            -- Khóa FPS 
-    HideMobVFX       = true,           -- Ẩn hiệu ứng chiêu thức/vũ khí của mob & boss
+    FPSLock          = 10,            -- Khóa FPS 
+    HideMobVFX       = false,           -- Ẩn hiệu ứng chiêu thức/vũ khí của mob & boss
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1169527463a6fb002d07c345110bc0aa.lua"))()
