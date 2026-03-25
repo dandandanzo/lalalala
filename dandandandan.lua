93aacc3f451b23cb9f6871f867e4aca703bf8ae4
dandandanzo <jrdntio@gmail.com>
2026-03-25 15:07:55 +0800

    Update dandandandan.lua

--- dandandandan.lua
+++ dandandandan.lua
@@ -1,17 +1,38 @@
 script_key="hsyQEYXbFmWuTvLhCbxxwtIZfsDgpMus";
-getgenv().SailorPieceConfig = {
-    AutoRejoinIfHighRam = 5000,
-    OpenChests       = {              -- Danh sách chest muốn mở
+getgenv().SailorPieceConfig = getgenv().SailorPieceConfig or {
+    OpenChests       = {
         "Common Chest",
         "Rare Chest",
         "Epic Chest",
         "Legendary Chest",
-        "Mythical Chest",
     },
-    TargetRace       = "Kitsune",     -- Race mục tiêu (dừng khi đạt)
-    TargetClan       = "Monarch",     -- Clan mục tiêu (dừng khi đạt)
-    WebhookURL       = "https://discord.com/api/webhooks/1478134572867911691/Z5WpUhdJkcdBSsoYE-k79YovaVsxNn93aN7TqRrGu7VFEXh_Lsm0znIaRln5DNbSquoG",             -- Discord Webhook URL (để trống = tắt)
-    FPSBoost         = true,          -- Tối ưu FPS (giảm lag)
-    FPSLock          = 10,            -- Khóa FPS 
+    Debug            = false,
+    Performance = {
+        AutoRejoinIfHighRam = 5000,
+        WebhookURL      = "https://discord.com/api/webhooks/1478134572867911691/Z5WpUhdJkcdBSsoYE-k79YovaVsxNn93aN7TqRrGu7VFEXh_Lsm0znIaRln5DNbSquoG",
+        FPSBoost        = true,
+        FPSLock         = 10,
+    },
+    SkillTree = {
+        Auto            = true,
+        Priority        = {"Luck", "Damage", "CritChance", "CritDamage", "HP"},
+    },
+    AutoStrongestMelee = true,
+    Reroll = {
+        Trait = {
+            Auto            = true,
+            TargetRarity    = "Secret",
+        },
+        TargetRace      = "Kitsune",
+        TargetClan      = "Monarch",
+    },
+    Artifact = {
+        AutoEquip       = true,
+        AutoUpgrade     = true,
+        AutoDeleteTrash = true,
+        UpgradeSet      = {"Celestial Rupture"},
+        UpgradeStopAt   = 15,
+        TrashRarities   = {"Common", "Rare", "Epic"},
+    },
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1169527463a6fb002d07c345110bc0aa.lua"))()
